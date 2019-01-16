1866215fecf72a2c5595398482fd16c6671ef8e2
Yuxuan Yao <yuxuanyyx@gmail.com>
2019-01-15 22:04:28 -0500

    added routes and items

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://yuxuan:abc123@ds035593.mlab.com:35593/mern_shopping'
+}
---
+++ routes/api/items.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Item = require('../../models/Item');
+
+// @route GET api/items
+// @desc  Get All Items
+// @access   Public
+
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({ date: -1 })
+        .then(items => res.json(items));
+});
+
+
+module.exports = router;
